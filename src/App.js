3498fe1b3a37cb8ba397b876a5e32d6cc152f6b1
05nam <namk14bk@gmail.com>
2023-04-22 01:30:09 +0700

    lý do thêm code

--- src/App.js
+++ src/App.js
@@ -10,7 +10,27 @@ import ProjectManager from './pages/projectManager/ProjectManager';
 import User from './pages/users/User';
 import CreateTask from './pages/components/CreateTask';
 import ProjectDetail from './pages/projectDetail/ProjectDetail';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCxst0yxug7VXfEhWfI4IKx-lqINt0RsxM",
+  authDomain: "jara-e80af.firebaseapp.com",
+  projectId: "jara-e80af",
+  storageBucket: "jara-e80af.appspot.com",
+  messagingSenderId: "904586310055",
+  appId: "1:904586310055:web:a3df5ebbbb5bec21c70682",
+  measurementId: "G-QTX8SJY82X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const history = createBrowserHistory()
 function App() {
